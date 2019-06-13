ebc47765ad22583f082fbff23c86b024c1f7927b
Markus Strömgren <markus.stromgren@dialect.se>
2019-06-13 12:40:43 +0200

    Removed laravel-model-caching (#22)
    * Removed laravel-model-caching
    
    * Apply fixes from StyleCI (#23)
    
    [ci skip] [skip ci]

--- src/Models/Key.php
+++ src/Models/Key.php
@@ -3,12 +3,9 @@
 namespace Dialect\TransEdit\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 
 class Key extends Model
 {
-    use Cachable;
-
     protected $table = 'transedit_keys';
     protected $guarded = ['id'];
 

a4805729a872aac72669bcb0999f8c4e4ddf43e4
Andreas Strömgren <andreas@stromgren.nu>
2022-10-14 09:21:43 +0200

    Apply fixes from StyleCI (#38)
    [ci skip] [skip ci]
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,7 @@
 if (! function_exists('transEdit')) {
     /**
      * Get an instance of TransEdit.
+     *
      * @return \Dialect\TransEdit\TransEdit
      */
     function transEdit($key = null, $val = null, $locale = 'current')
